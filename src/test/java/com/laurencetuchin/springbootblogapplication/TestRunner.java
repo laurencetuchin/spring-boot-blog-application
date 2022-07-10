c3c7cb744e446e44c4a6a11fe5dea85f57c4bf6f
laurencetuchin <laurencetuchin@gmail.com>
2022-07-11 07:06:02 +1000

    Initial commit

---
+++ src/test/java/com/laurencetuchin/springbootblogapplication/TestRunner.java
@@ -0,0 +1,15 @@
+package com.laurencetuchin.springbootblogapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRunner {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+}
