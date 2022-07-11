d250cc0fb65c1bd50738bfba05aa2757bd2fc466
laurencetuchin <laurencetuchin@gmail.com>
2022-07-11 17:54:24 +1000

    Create Post model

--- src/main/java/com/laurencetuchin/springbootblogapplication/models/Post.java
+++ src/main/java/com/laurencetuchin/springbootblogapplication/models/Post.java
@@ -3,11 +3,23 @@ package com.laurencetuchin.springbootblogapplication.models;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String body;
+
+    private LocalDateTime createdAt;
 
 }
