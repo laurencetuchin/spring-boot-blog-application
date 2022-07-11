1f86fc022ff26ba3c389a63e7b0c01869b63acea
laurencetuchin <laurencetuchin@gmail.com>
2022-07-11 17:51:19 +1000

    Create Post model

---
+++ src/main/java/com/laurencetuchin/springbootblogapplication/models/Post.java
@@ -0,0 +1,13 @@
+package com.laurencetuchin.springbootblogapplication.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+public class Post {
+
+}
