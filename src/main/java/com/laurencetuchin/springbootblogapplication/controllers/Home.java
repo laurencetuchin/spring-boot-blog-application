9fa7527804be7e52dd6724435e587cbe5fad712d
laurencetuchin <laurencetuchin@gmail.com>
2022-07-12 11:21:38 +1000

    Create home controller

--- src/main/java/com/laurencetuchin/springbootblogapplication/controllers/Home.java
+++ src/main/java/com/laurencetuchin/springbootblogapplication/controllers/Home.java
@@ -1,5 +1,26 @@
 package com.laurencetuchin.springbootblogapplication.controllers;
 
+import com.laurencetuchin.springbootblogapplication.models.Post;
+import com.laurencetuchin.springbootblogapplication.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class Home {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/")
+    public String home(Model model){
+        List<Post> posts = postService.getAll();
+        model.addAttribute("posts", posts);
+        return "home";
+    }
+
 
 }
