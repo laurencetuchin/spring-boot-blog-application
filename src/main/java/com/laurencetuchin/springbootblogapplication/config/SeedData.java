672fed790c4950f8cc958d8e75990beeaefce508
laurencetuchin <laurencetuchin@gmail.com>
2022-07-12 11:16:43 +1000

    Create Seed data

--- src/main/java/com/laurencetuchin/springbootblogapplication/config/SeedData.java
+++ src/main/java/com/laurencetuchin/springbootblogapplication/config/SeedData.java
@@ -1,5 +1,35 @@
 package com.laurencetuchin.springbootblogapplication.config;
 
-public class SeedData {
+import com.laurencetuchin.springbootblogapplication.models.Post;
+import com.laurencetuchin.springbootblogapplication.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@Component
+public class SeedData implements CommandLineRunner {
+
+    @Autowired
+    private PostService postService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<Post> posts = postService.getAll();
+
+        if (posts.size() == 0){
+            Post post1 = new Post();
+            post1.setTitle("Title of post 1");
+            post1.setBody("This is the body of post 1");
+
+            Post post2 = new Post();
+            post2.setTitle("Title of post 2");
+            post2.setBody("This is the body of post 2");
+
+            postService.save(post1);
+            postService.save(post2);
+
+        }
+    }
 }
