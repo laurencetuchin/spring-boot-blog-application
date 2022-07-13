c9b067f7cf301a870c7d727599dd19d1853e0439
laurencetuchin <laurencetuchin@gmail.com>
2022-07-13 11:47:29 +1000

    Inject acc repo into acc service

---
+++ src/main/java/com/laurencetuchin/springbootblogapplication/services/AccountService.java
@@ -0,0 +1,14 @@
+package com.laurencetuchin.springbootblogapplication.services;
+
+import com.laurencetuchin.springbootblogapplication.repositories.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+}
+
+
