e4d26137130f18f80459548f6711963c706c093a
laurencetuchin <laurencetuchin@gmail.com>
2022-07-13 11:46:15 +1000

    Create Account repository

---
+++ src/main/java/com/laurencetuchin/springbootblogapplication/repositories/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.laurencetuchin.springbootblogapplication.repositories;
+
+import com.laurencetuchin.springbootblogapplication.models.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
