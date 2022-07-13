b3398dc08afa293235cc9029412f061e7363b03b
laurencetuchin <laurencetuchin@gmail.com>
2022-07-13 12:16:57 +1000

    Create RegisterController

---
+++ src/main/java/com/laurencetuchin/springbootblogapplication/controllers/RegisterController.java
@@ -0,0 +1,15 @@
+package com.laurencetuchin.springbootblogapplication.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegisterController {
+
+    @GetMapping("/register")
+    public String getRegisterPage() {
+        return "register";
+    }
+
+
+}
