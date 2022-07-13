9036469db846e76e5d06ac907427b1f00c3f02ff
laurencetuchin <laurencetuchin@gmail.com>
2022-07-13 12:13:47 +1000

    Refactor renamed HomeController from home

--- src/main/java/com/laurencetuchin/springbootblogapplication/controllers/HomeController.java
+++ src/main/java/com/laurencetuchin/springbootblogapplication/controllers/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @Controller
-public class Home {
+public class HomeController {
 
     @Autowired
     private PostService postService;
