dd3d304e02f609ab21a2b03d854fd907b7350b09
laurencetuchin <laurencetuchin@gmail.com>
2022-07-13 17:07:09 +1000

    Add security config

---
+++ src/main/java/com/laurencetuchin/springbootblogapplication/config/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.laurencetuchin.springbootblogapplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class WebSecurityConfig {
+
+    private static final String[] WHITELIST = {
+            "/register",
+            "/login",
+            "h2-console/*",
+            "/"
+
+
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(WHITELIST).permitAll()
+                .antMatchers(HttpMethod.GET, "/posts/*")
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+}
