4cf681c2d42978e84e925391ca2634ca9e8c0ce4
laurencetuchin <laurencetuchin@gmail.com>
2022-07-13 15:15:01 +1000

    Create register POST mapping

--- src/main/java/com/laurencetuchin/springbootblogapplication/controllers/RegisterController.java
+++ src/main/java/com/laurencetuchin/springbootblogapplication/controllers/RegisterController.java
@@ -1,15 +1,29 @@
 package com.laurencetuchin.springbootblogapplication.controllers;
 
+import com.laurencetuchin.springbootblogapplication.models.Account;
+import com.laurencetuchin.springbootblogapplication.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
+
+    @Autowired
+    private AccountService accountService;
 
     @GetMapping("/register")
     public String getRegisterPage() {
         return "register";
     }
 
+    @PostMapping("/register")
+    public String registerNewUser(@ModelAttribute Account account){
+        accountService.save(account);
+
+        return "redirect:/";
+    }
 
 }
