6b4b4b7f4caef125afde91e36647a4e8c73a69d5
laurencetuchin <laurencetuchin@gmail.com>
2022-07-13 11:49:22 +1000

    add save account feature

--- src/main/java/com/laurencetuchin/springbootblogapplication/services/AccountService.java
+++ src/main/java/com/laurencetuchin/springbootblogapplication/services/AccountService.java
@@ -1,5 +1,6 @@
 package com.laurencetuchin.springbootblogapplication.services;
 
+import com.laurencetuchin.springbootblogapplication.models.Account;
 import com.laurencetuchin.springbootblogapplication.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,10 @@ public class AccountService {
 
     @Autowired
     private AccountRepository accountRepository;
+
+    public Account save(Account account) {
+        return accountRepository.save(account);
+    }
 }
 
 
